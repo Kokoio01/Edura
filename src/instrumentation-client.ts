3c8e44cc2583c3cbc146c8711438405a7f19c110
Lukas <210173666+Kokoio01@users.noreply.github.com>
2025-09-10 18:31:20 +0000

    fix(stats) posthog host url

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,9 +1,12 @@
 import posthog from "posthog-js"
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://eu.posthog.com",
-  defaults: '2025-05-24',
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking
-  debug: process.env.NODE_ENV === "development",
-});
+const key = process.env.NEXT_PUBLIC_POSTHOG_KEY
+const host = process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com"  
+
+if (typeof window !== "undefined" && key) {  
+  posthog.init(key, {  
+    api_host: host,  
+    capture_exceptions: true,  
+    debug: process.env.NODE_ENV === "development",  
+  })  
+}  
