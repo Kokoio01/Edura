2e7cfce4a111a7cd09c2d965afbab745404f1e61
Lukas <210173666+Kokoio01@users.noreply.github.com>
2026-01-19 18:13:55 +0100

    fix: Docker and proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,18 +1,19 @@
 import { auth } from "@/lib/auth";
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function proxy(req: NextRequest) {
-    const session = await auth.api.getSession({ headers: req.headers });
-    const userId = session?.user?.id ?? null;
-    if (!userId) {
-        const {pathname} = req.nextUrl;
-        const encodedPath = encodeURIComponent(pathname);
-        const loginUrl = new URL(`/login?next=${encodedPath}`, req.url);
-        return NextResponse.redirect(loginUrl);
-    }
-    return NextResponse.next();
+  const session = await auth.api.getSession({ headers: req.headers });
+  const userId = session?.user?.id ?? null;
+  if (!userId) {
+    const { pathname } = req.nextUrl;
+    const encodedPath = encodeURIComponent(pathname);
+    const loginUrl = new URL(`/login?next=${encodedPath}`, req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+  return NextResponse.next();
 }
 export const config = {
-    matcher: ['/dashboard/:path*', '/subject/:path*', '/settings/:path*']
+  matcher: ["/dashboard/:path*", "/subject/:path*", "/settings/:path*"],
+  runtime: "nodejs",
 };
